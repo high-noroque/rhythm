0e9431c97a286150ca0f1506997597c602d48e60
Mihail Ciobanu <positronmihai@gmail.com>
2018-09-14 15:47:15 +0300

    Router set up

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react'
+
+class Register extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Register</h1>
+      </Fragment>
+    )
+  }
+}
+
+export default Register
